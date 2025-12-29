3bbe709f4d097ccf22c8a43167eae8058b70ab3b
Shami2810 <shami.ssk2810@gmail.com>
2025-12-29 10:48:40 +0530

    learn terraform code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -24,25 +24,25 @@ resource "aws_route53_record" "public" {
   ttl     = 300
   records = [aws_instance.instance.public_ip]
 }
-
-resource "null_resource" "ansible" {
-  triggers = {
-    always = var.env  == null ? timestamp() : "false"
-  }
-
-  depends_on = [aws_route53_record.record]
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = "DevOps321"
-      host     = aws_instance.instance.private_ip
-    }
-
-    inline = [
-      "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/shamidevsecops/roboshop-ansible roboshop.yml -e role_name=${var.name}"
-    ]
-  }
-}
+#
+# resource "null_resource" "ansible" {
+#   triggers = {
+#     always = var.env  == null ? timestamp() : "false"
+#   }
+#
+#   depends_on = [aws_route53_record.record]
+#   provisioner "remote-exec" {
+#     connection {
+#       type     = "ssh"
+#       user     = "ec2-user"
+#       password = "DevOps321"
+#       host     = aws_instance.instance.private_ip
+#     }
+#
+#     inline = [
+#       "sudo pip3.11 install ansible",
+#       "ansible-pull -i localhost, -U https://github.com/shamidevsecops/roboshop-ansible roboshop.yml -e role_name=${var.name}"
+#     ]
+#   }
+# }
 
