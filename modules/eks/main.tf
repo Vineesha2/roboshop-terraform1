be9414bf15fe4d67a2004a0a9c062932f6c95c6f
Shami2810 <shami.ssk2810@gmail.com>
2026-01-05 23:35:05 +0530

    RoboShop code

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,97 +1,116 @@
+################################
+# EKS CLUSTER
+################################
 resource "aws_eks_cluster" "main" {
-  name = "example"
-
+  name     = "example"
   role_arn = aws_iam_role.cluster.arn
   version  = "1.31"
+
   vpc_config {
-    subnet_ids = ["subnet-08b0162e299f30092","subnet-0c69bfe9bbe3bdf8a","subnet-03e1c6cf17f0614d8"]
-  }
-}
-
-resource "aws_iam_role" "node" {
-  name = "eks-auto-node-example"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = ["sts:AssumeRole"]
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
+    subnet_ids = [
+      "subnet-08b0162e299f30092",
+      "subnet-0c69bfe9bbe3bdf8a",
+      "subnet-03e1c6cf17f0614d8"
     ]
-  })
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSComputePolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSBlockStoragePolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSLoadBalancingPolicy,
+    aws_iam_role_policy_attachment.cluster_AmazonEKSNetworkingPolicy,
+  ]
 }
 
-resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
-  role       = aws_iam_role.node.name
-}
-
-resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
-  role       = aws_iam_role.node.name
-}
-
+################################
+# IAM ROLE – CLUSTER
+################################
 resource "aws_iam_role" "cluster" {
   name = "eks-cluster-example"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = ["sts:AssumeRole", "sts:TagSession"]
+    }]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSComputePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSComputePolicy"
   role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSComputePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSBlockStoragePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy"
   role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSLoadBalancingPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
   role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSNetworkingPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy"
   role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy"
 }
 
+################################
+# IAM ROLE – NODE
+################################
+resource "aws_iam_role" "node" {
+  name = "eks-node-example"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_WorkerNodePolicy" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_ECR" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "node_CNI" {
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+################################
+# NODE GROUP
+################################
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "nodeg-01"
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids = ["subnet-08b0162e299f30092", "subnet-0c69bfe9bbe3bdf8a"]
+
+  subnet_ids = [
+    "subnet-08b0162e299f30092",
+    "subnet-0c69bfe9bbe3bdf8a"
+  ]
 
   scaling_config {
     desired_size = 1
-    max_size     = 2
     min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
+    max_size     = 2
   }
 }
-
